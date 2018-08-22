c92f4515861041e8fa01e15f6248cf7a0b058059
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-08-22 08:52:56 +0300

    Add initial tests

---
+++ methods.js
@@ -0,0 +1,23 @@
+module.exports = {
+  firstNamesOnly(students) {
+    return students;
+  },
+  fullNamesOnly(students) {
+    return students;
+  },
+  ariNotSuspended(students) {
+    return students;
+  },
+  gradesBelow8(students) {
+    return students;
+  },
+  gradesBelow8orOver9(students) {
+    return students;
+  },
+  collectiveGrade(students) {
+    return students;
+  },
+  groupedByGrade(students) {
+    return students;
+  },
+}
---
+++ methods.test.js
@@ -0,0 +1,73 @@
+const methods = require('./methods');
+
+const students = [
+  { name: { first: 'Henrik', last: 'Pekkonen' }, grade: 9, suspended: false },
+  { name: { first: 'Aleksi', last: 'Jääskeläinen' }, grade: 8, suspended: false },
+  { name: { first: 'Ari', last: 'Liukkonen' }, grade: 7, suspended: true },
+  { name: { first: 'Erik', last: 'Heino' }, grade: 8, suspended: false },
+  { name: { first: 'Bhanu', last: 'Rantanen' }, grade: 8, suspended: true },
+  { name: { first: 'Besserwisser', last: 'Ranta' }, grade: 10, suspended: false }
+];
+
+test('array of strings first name', () => {
+  expect(methods.firstNamesOnly(students)).toEqual([
+    'Henrik',
+    'Aleksi',
+    'Ari',
+    'Erik',
+    'Bhanu',
+    'Besserwisser'
+  ]);
+});
+
+test('array of strings with full name', () => {
+  expect(methods.fullNamesOnly(students)).toEqual([
+    'Henrik Pekkonen',
+    'Aleksi Jääskeläinen',
+    'Ari Liukkonen',
+    'Erik Heino',
+    'Bhanu Rantanen',
+    'Besserwisser Ranta'
+  ]);
+});
+
+test('get students buts Ari Liukkonen is not suspended anymore', () => {
+  expect(methods.ariNotSuspended(students)).toEqual([
+    { name: { first: 'Henrik', last: 'Pekkonen' }, grade: 9, suspended: false },
+    { name: { first: 'Aleksi', last: 'Jääskeläinen' }, grade: 8, suspended: false },
+    { name: { first: 'Ari', last: 'Liukkonen' }, grade: 7, suspended: false },
+    { name: { first: 'Erik', last: 'Heino' }, grade: 8, suspended: false },
+    { name: { first: 'Bhanu', last: 'Rantanen' }, grade: 8, suspended: true },
+    { name: { first: 'Besserwisser', last: 'Ranta' }, grade: 10, suspended: false }
+  ]);
+});
+
+test('array containing only students with grade below 8', () => {
+  expect(methods.gradesBelow8(students)).toEqual([
+    { name: { first: 'Ari', last: 'Liukkonen' }, grade: 7 }
+  ]);
+});
+
+test('array containing only students with grade below 8 or over 9', () => {
+  expect(methods.gradesBelow8orOver9(students)).toEqual([
+    { name: { first: 'Ari', last: 'Liukkonen' }, grade: 7 },
+    { name: { first: 'Besserwisser', last: 'Ranta' }, grade: 10 }
+  ]);
+});
+
+test('all grades added together', () => {
+  expect(methods.collectiveGrade(students)).toEqual(50);
+});
+
+test('students grouped by grade', () => {
+  expect(methods.groupedByGrade(students)).toEqual({
+    7: [{ name: { first: 'Ari', last: 'Liukkonen' }, grade: 7 }],
+    8: [
+      { name: { first: 'Aleksi', last: 'Jääskeläinen' }, grade: 8 },
+      { name: { first: 'Erik', last: 'Heino' }, grade: 8 },
+      { name: { first: 'Bhanu', last: 'Rantanen' }, grade: 8 }
+    ],
+    9: [{ name: { first: 'Henrik', last: 'Pekkonen' }, grade: 9 }],
+    10: [{ name: { first: 'Besserwisser', last: 'Ranta' }, grade: 10 }]
+  });
+});
