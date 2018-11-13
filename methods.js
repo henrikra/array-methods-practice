2fffdf6b5453c7e7d74f1f0a32ac3c26fbbd64d7
Sami H <samih90@gmail.com>
2018-11-13 19:28:12 +0200

    Commit

--- methods.js
+++ methods.js
@@ -1,23 +1,56 @@
 module.exports = {
   firstNamesOnly(students) {
-    return students;
+    const studentNames = [];
+    students.forEach(element => {
+      studentNames.push(element.name.first)
+    });
+    return studentNames;
   },
+
   fullNamesOnly(students) {
-    return students;
+    const studentNames = [];
+    students.forEach(element => {
+      const firstName = element.name.first;
+      const lastName = element.name.last;
+      const fullName = firstName.concat(' ', lastName);
+      studentNames.push(fullName);
+    })
+    return studentNames;
   },
+
   ariNotSuspended(students) {
-    return students;
+    const newArray = JSON.parse(JSON.stringify(students));
+    newArray[2].suspended = false;
+    return newArray;
   },
+
   gradesBelow8(students) {
-    return students;
+    const newArray = students.filter(e => e.grade < 8);
+    return newArray;
   },
   gradesBelow8orOver9(students) {
-    return students;
+    const newArray = students.filter(e => e.grade < 8 || e.grade > 9);
+    return newArray;
   },
   collectiveGrade(students) {
-    return students;
+    const reducer = students.reduce(function(a, b) {
+      console.log('accumulative ', a, ' current ', b);
+      return a + b.grade;
+    }, 0);
+    return reducer;
   },
   groupedByGrade(students) {
-    return students;
+    const newArray = [...students];
+    const sevens = newArray.filter(e => e.grade === 7);
+    const eights = newArray.filter(e => e.grade === 8);
+    const ninths = newArray.filter(e => e.grade === 9);
+    const tens = newArray.filter(e => e.grade === 10);
+    const finalObject = {
+      7 : sevens,
+      8 : eights,
+      9 : ninths,
+      10 : tens
+    };
+   return finalObject;
   },
 }
