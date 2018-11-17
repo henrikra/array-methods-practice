7693813d7d32a5d175019180c0fb9ff4c843de27
Sami H <samih90@gmail.com>
2018-11-17 17:42:08 +0200

    Fixed a bunch of functions

--- methods.js
+++ methods.js
@@ -1,56 +1,45 @@
 module.exports = {
   firstNamesOnly(students) {
-    const studentNames = [];
-    students.forEach(element => {
-      studentNames.push(element.name.first)
+    const nameArray = students.map(function(element) {
+      return element.name.first;
+    });
+    return nameArray;
+  },
+
+  fullNamesOnly(students) {
+    const studentNames = students.map(function(student) {
+      return student.name.first.concat(' ', student.name.last);
     });
     return studentNames;
   },
 
-  fullNamesOnly(students) {
-    const studentNames = [];
-    students.forEach(element => {
-      const firstName = element.name.first;
-      const lastName = element.name.last;
-      const fullName = firstName.concat(' ', lastName);
-      studentNames.push(fullName);
-    })
-    return studentNames;
-  },
-
   ariNotSuspended(students) {
-    const newArray = JSON.parse(JSON.stringify(students));
-    newArray[2].suspended = false;
-    return newArray;
+    let name = 'Ari'
+    const ariModified = students.map(student => {
+      if (student.name.first === name) {
+        student = { ...student, suspended: false };
+      }
+      return student;
+    });
+    return ariModified;
   },
 
   gradesBelow8(students) {
-    const newArray = students.filter(e => e.grade < 8);
+    const newArray = students.filter(student => student.grade < 8);
     return newArray;
   },
   gradesBelow8orOver9(students) {
-    const newArray = students.filter(e => e.grade < 8 || e.grade > 9);
+    const newArray = students.filter(student => student.grade < 8 || student.grade > 9);
     return newArray;
   },
   collectiveGrade(students) {
-    const reducer = students.reduce(function(a, b) {
-      console.log('accumulative ', a, ' current ', b);
+    const massGrade = students.reduce(function(a, b) {
       return a + b.grade;
     }, 0);
-    return reducer;
+    return massGrade;
   },
   groupedByGrade(students) {
-    const newArray = [...students];
-    const sevens = newArray.filter(e => e.grade === 7);
-    const eights = newArray.filter(e => e.grade === 8);
-    const ninths = newArray.filter(e => e.grade === 9);
-    const tens = newArray.filter(e => e.grade === 10);
-    const finalObject = {
-      7 : sevens,
-      8 : eights,
-      9 : ninths,
-      10 : tens
-    };
-   return finalObject;
-  },
+    const result = students.reduce((acc, curr) => Object.assign(acc, { [curr.grade]: (acc[curr.grade] || []).concat(curr) }), {});
+    return result;
+  }
 }
